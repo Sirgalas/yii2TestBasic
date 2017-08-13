0e7138cd01ca47a5ca8355ab2cabd7b5391b68c7
sirgalas <hudos@rambler.ru>
2017-08-14 00:14:36 +0300

    add migration

---
+++ migrations/m170813_205400_projeckt.php
@@ -0,0 +1,66 @@
+<?php
+
+use yii\db\Migration;
+
+class m170813_205400_projeckt extends Migration
+{
+    public function safeUp()
+    {
+
+    }
+
+    public function safeDown()
+    {
+        echo "m170813_205400_projeckt cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.*/
+    public function up()
+    {
+        $this->createTable('{{%image}}',[
+            'id'=>$this->primaryKey(),
+            'id_post'=>$this->integer()->notNull(),
+            'path '=>$this->string(610)->notNull(),
+            'name'=>$this->string(610)->notNull(),
+        ]) ;
+
+        $this->createTable('message_user',[
+            'id'=>$this->primaryKey(),
+            'id_user '=>$this->integer()->notNull(),
+            'viewd'=>'ENUM("не прочитано", "прочитано")',
+            'fromMessage'=>$this->integer()->notNull(),
+            'subject '=>$this->string(255)->notNull(),
+            'text'=>$this->text()->notNull(),
+            'preview'=>$this->string(610),
+            'id_post'=>$this->integer()->notNull(),
+
+        ]) ;
+        $sqlMessage = "ALTER TABLE message_user ALTER is_active SET DEFAULT 'не прочитано'";
+        $this->execute($sqlMessage);
+
+        $this->createTable('post',[
+            'id'=>$this->primaryKey(),
+            'title'=>$this->string(255)->notNull(),
+            'alias'=>$this->string(610)->notNull(),
+            'content'=>$this->text()->notNull(),
+            'preview'=>$this->string(610),
+            'create_at'=>$this->integer()->notNull(),
+            'update_at'=>$this->integer()->notNull(),
+            'autor_id '=>$this->integer()->notNull(),
+            'status'=>'ENUM("active", "blocked")',
+        ]) ;
+        $sqlPost = "ALTER TABLE post ALTER is_active SET DEFAULT 'active'";
+        $this->execute($sqlPost);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%image}}');
+        $this->dropTable('message_user');
+        $this->dropTable('post');
+    }
+
+}
